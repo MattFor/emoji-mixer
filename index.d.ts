fa419cfdd19dd79d8d2f57b1187d462b20a5ed19
MattFor <matthew-forester@protonmail.com>
2024-02-15 23:57:44 +0100

    1.0.51

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@
 
 /**
  * `Emoji Mix URL Generator` TypeScript Definitions
- * Version 1.0.5
+ * Version 1.0.51
  *
  * Created by MattFor (Discord: MattFor#9884) on May 30, 2023.
  * Contact: matthew-forester@protonmail.com
